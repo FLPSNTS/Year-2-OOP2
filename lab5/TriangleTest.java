1020a49a7ccc2a12a8f3a9343cdc07c8aaa7b306
filippi <c7370821@leedsbeckett.ac.uk>
2025-12-03 14:54:27 +0000

    lab 5 created triangletest needs to populate it

---
+++ lab5/TriangleTest.java
@@ -0,0 +1,39 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TriangleTest {
+
+    @BeforeEach
+    void setUp() {}
+
+    @AfterEach
+    void tearDown() {}
+
+    @Test
+    void isIsosceles() {}
+
+    @Test
+    void isEquilateral() {}
+
+    @Test
+    void isScalene() {}
+
+    @Test
+    void setSides() {}
+
+    @Test
+    void testSetSides() {}
+
+    @Test
+    void testSetSides1() {}
+
+    @Test
+    void copy() {}
+
+    @Test
+    void getPerimeter() {}
+
+    @Test
+    void getAverageLength() {}
+}
