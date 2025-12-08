5a9d8b6530d038b52bcc4aa76880b20fbb0e374f
Filippi Santos <c7370821@leedsbeckett.ac.uk>
2025-12-08 09:31:17 +0000

    big o notations

--- lab9_ac/NumberChecker.java
+++ lab9_ac/NumberChecker.java
@@ -4,16 +4,19 @@ public class NumberChecker {
     int findMax(Integer[] numbers) {
 
         // assume first element is the max
-        int max = numbers[0];
+        int max = numbers[0]; // O
 
         // loop through remaining numbers
-        for (int i = 1; i < numbers.length; i++) {
+        for (int i = 1; i < numbers.length; i++) { // BIG O : O(n) -> a loop
             if (numbers[i] > max) {
                 max = numbers[i];
             }
         }
 
         return max;
+
+        //growth : T(n) = c₁ (O) + (c₂·n [from the loop])
+
     }
 
 }
