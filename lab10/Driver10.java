7d8c412018564dd194e119ea603f8be5037b670b
Filippi Santos <c7370821@leedsbeckett.ac.uk>
2025-12-08 10:38:39 +0000

    lab 10 update

--- lab10/Driver10.java
+++ lab10/Driver10.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Driver10 {
 
@@ -34,8 +36,42 @@ public class Driver10 {
                                 .sorted()
                                         .toArray(); // converting from object to array
 
-
         System.out.println(Arrays.toString(studentArray));
+
+
+        double average = students.stream()
+                        .mapToDouble(Student :: getGrade) // maping each Student object to their grade (done in background)
+                                .average() // gets the average of all the mapped grades
+                                        .getAsDouble(); // converts to a double
+
+        System.out.println(average);
+
+        IntStream.range(90, 101)
+                .map(n -> n * n * n)
+                .forEach(System.out::println);
+
+        Stream.generate(Math::random) // infinite random doubles
+                .map(n -> (int)(n * 5 + 1)) // intergers 1-5
+                .limit(10)
+                .forEach(System.out::println);
+
+
+//
+//        Collect the values into a List of
+//        integer values, then display the list using a traditional for
+//        loop, e.g.
+//                List<Integer> numList = …stream methods
+//        for(int next : numList)
+//            System.out.println("Next value is : " + next);
+//        3. What additional intermediate method could be added to the
+//        above statement to ensure any duplicate values were removed
+//        from the resultant collection? Add this to the solution.
+
+
+
+
+
+
 
 
     }
